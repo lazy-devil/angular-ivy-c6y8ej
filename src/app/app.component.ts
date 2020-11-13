b04adc2bd2350f9a9ff07898533ef6442c8a2999
lazy-devil <niran.p@hotmail.com>
2020-11-13 15:18:19 +0700

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = "Drag & Drop in Angular 7";
+
+  todos = [
+    {
+      name: "Angular",
+      category: "Web Development"
+    },
+    {
+      name: "Flexbox",
+      category: "Web Development"
+    },
+    {
+      name: "iOS",
+      category: "App Development"
+    },
+    {
+      name: "Java",
+      category: "Software development"
+    }
+  ];
+
+  completed = [
+    {
+      name: "Android",
+      category: "Mobile Development"
+    },
+    {
+      name: "MongoDB",
+      category: "Databases"
+    },
+    {
+      name: "ARKit",
+      category: "Augmented Reality"
+    },
+    {
+      name: "React",
+      category: "Web Development"
+    }
+  ];
+
+  //onDrop(event: CdkDragDrop<string[]>) {
+  onDrop(event: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { MatCardModule } from "@angular/material/card";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    DragDropModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
